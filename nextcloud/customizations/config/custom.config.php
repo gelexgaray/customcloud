ff5aca72508bc4adba5d451f5985730f4fdcdeb8
Gorka Elexgaray <gelexgaray@yahoo.es>
2021-08-29 01:01:01 +0200

    Enable breeze dark theme by default. Solve out of memory problems when running OCC commands during startup

--- nextcloud/customizations/config/custom.config.php
+++ nextcloud/customizations/config/custom.config.php
@@ -1,4 +1,8 @@
 <?php
 $CONFIG = array(
-  'default_phone_region' => 'ES'
+  'default_phone_region' => 'ES',
+  
+  # Disable debug to avoid low mem errors: https://github.com/nextcloud/server/issues/25742
+  'debug' => 'false',
+  'loglevel' => 2,
 );
--- nextcloud/entrypoint.sh
+++ nextcloud/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Globallly export $OCC to use as an alias of occ command
+export OCC="sudo -E -u www-data /var/www/html/occ"
+
 # Put here code to run before nextcloud installation/upgrade
 echo "Before nextcloud installation customizations"
 
@@ -12,6 +15,13 @@ echo "Copy customizations over nextcloud data volume"
 cp -R /tmp/customizations/* /var/www/html/
 chown -R www-data /var/www/html/*
 chmod 755 /var/www/html/*
+
+echo "Enable breezedark theme"
+# When running occ with sudo, -E must be used to pass PHP_MEMORY_LIMIT 
+# and the rest of the main environament into www-data environment
+# and avoid out of memory errors
+$OCC app:enable breezedark
+$OCC config:app:set breezedark theme_enabled --value 1
 
 # Finally, here goes the real exec of the server
 exec "$@" 

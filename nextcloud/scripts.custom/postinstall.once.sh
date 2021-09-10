cb85d7a6c73448ca75f6a5bb1a1101731225303e
Gorka Elexgaray <gelexgaray@virtualwareco.com>
2021-09-10 16:32:55 +0200

    Prepare for keycloak customization and add SAML plugin to nextcloud

--- nextcloud/scripts.custom/postinstall.once.sh
+++ nextcloud/scripts.custom/postinstall.once.sh
@@ -4,3 +4,13 @@
 echo "Enable breezedark theme"
 occ app:enable breezedark
 occ config:app:set breezedark theme_enabled --value 1
+
+echo "Disable unused tabs"
+occ app:disable firstrunwizard
+occ app:disable activity
+
+echo "Disable status notification"
+occ app:disable user_status
+
+echo "Download and enable SAML integration app"
+occ app:install user_saml

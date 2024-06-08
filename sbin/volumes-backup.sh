c7eb0ff597b73e6dfd188c3c2cbc7de6f8d99978
Gorka Elexgaray <15904363+gelexgaray@users.noreply.github.com>
2024-06-08 16:57:10 +0200

    Add a simple volume backup script

---
+++ sbin/volumes-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/env bash
+COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
+BASENAME="$( basename $COMPOSE_DIR )"
+
+# Start on a known base dir
+cd "$COMPOSE_DIR"
+
+# Default backup folder. Can be overriden on the .env file
+BACKUP_FOLDER="./backup"
+if [ -f .env ]
+then
+  export $(cat .env | grep BACKUP_FOLDER | xargs)
+fi
+
+# Derive a destination folder
+echo "Prepare for backup..."
+DEST="."
+DATE=$( date -I'date' )
+mkdir "$BACKUP_FOLDER/" 2> /dev/null
+mkdir "$BACKUP_FOLDER/$DATE" 2> /dev/null
+
+docker compose down
+
+# Loop through volumes a backup each of them
+VOLUMES=$( docker volume ls --format "{{lower .Name}}" | grep "$BASENAME" )
+for VOLUME in $VOLUMES;
+do
+  echo "Backing up [$VOLUME]..."
+  # Foreach volume, backup using busybox
+  docker run --rm \
+    --mount source=$VOLUME,target=/mnt/source \
+    -v "$BACKUP_FOLDER/$DATE":/mnt/destination \
+    busybox \
+    tar -czvf /mnt/destination/$VOLUME.tar.gz -C /mnt/source . 
+done
+
+docker compose up -d
